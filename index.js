2abc335f2f1028c25572e00383b9486227d81642
vickyyede28 <vicky20@navgurukul.org>
2022-10-04 14:51:08 +0530

    UnitTestCasesThroughJest

---
+++ index.js
@@ -0,0 +1,71 @@
+const Hapi = require('@hapi/hapi');
+const users = [
+    { id: 1, name: 'Siva' },
+    { id: 2, name: 'Sivasankar' },
+    { id: 3, name: 'Niralar' }
+]
+
+// Create server listening on port 3000
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return 'Hello World!';
+    }
+});
+
+// server.route({
+//     method: 'POST',
+//     path: '/user',
+//     handler: (request, h) => {
+//         if (request.payload && request.payload.name) {
+//             let checkUser = users.find(user => user.name === request.payload.name);
+//             if (checkUser) {
+//                 return h.response('User already exists').code(422);
+//             }
+//             return h.response('User added successfully').code(200);
+//         } else {
+//             return h.response('Invalid user').code(422);
+//         }
+//     }
+// });
+
+// server.route({
+//     method: 'GET',
+//     path: '/user/{id}',
+//     handler: (request, h) => {
+//         let user = users.find(user => user.id === parseInt(request.params.id));
+//         if (!user) {
+//             return h.response('User not exists').code(422);
+//         }
+//         return user;
+//     }
+// });
+
+// server.route({
+//     method: 'GET',
+//     path: '/user/list',
+//     handler: (request, h) => {
+//         return users;
+//     }
+// });
+
+// Initiate the server
+const init = async () => {
+    await server.start();
+    console.log('Server running on %ss', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
+module.exports = server;
---
+++ test.js
@@ -0,0 +1,76 @@
+const server = require('./index.js'); // Import Server/Application
+
+// Start application before running the test case
+beforeAll((done) => {
+    server.events.on('start', () => {
+        done();
+    });
+});
+
+// Stop application after running the test case
+afterAll((done) => {
+    server.events.on('stop', () => {
+        done();
+    });
+    server.stop();
+});
+
+test('should success with server connection', async function () {
+    const options = {
+        method: 'GET',
+        url: '/'
+    };
+    const data = await server.inject(options);
+    expect(data.statusCode).toBe(200);
+});
+
+// test('should fail in adding user due to no payload', async function () {
+//     const options = {
+//         method: 'POST',
+//         url: '/user',
+//     };
+//     const data = await server.inject(options);
+//     expect(data.statusCode).toBe(422);
+//     expect(data.result).toBe('Invalid user');
+// });
+
+// test('should fail in adding user, where user already exists', async function () {
+//     const options = {
+//         method: 'POST',
+//         url: '/user',
+//         payload: JSON.stringify({ name: 'Siva' })
+//     };
+//     const data = await server.inject(options);
+//     expect(data.statusCode).toBe(422);
+//     expect(data.result).toBe('User already exists');
+// });
+
+// test('should add user successfully', async function () {
+//     const options = {
+//         method: 'POST',
+//         url: '/user',
+//         payload: JSON.stringify({ name: 'Test' })
+//     };
+//     const data = await server.inject(options);
+//     expect(data.statusCode).toBe(200);
+//     expect(data.result).toBe('User added successfully');
+// });
+
+// test('should get user successfully', async function () {
+//     const data = await server.inject('/user/3');
+//     expect(data.statusCode).toBe(200);
+//     expect(data.result.id).toBe(3);
+//     expect(data.result.name).toBe('Niralar');
+// });
+
+// test('should fail in getting user', async function () {
+//     const data = await server.inject('/user/5');
+//     expect(data.statusCode).toBe(422);
+//     expect(data.result).toBe('User not exists');
+// });
+
+// test('should list user successfully', async function () {
+//     const data = await server.inject('/user/list');
+//     expect(data.statusCode).toBe(200);
+//     expect(data.result.length).toBe(3);
+// });
